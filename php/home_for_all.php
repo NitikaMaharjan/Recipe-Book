93b437e3038782a47dfa9199bb97777a407e2a6c
Keppiekip <maharjanitika@gmail.com>
2024-11-07 17:57:38 +0545

    home for all page

--- php/home_for_all.php
+++ php/home_for_all.php
@@ -27,13 +27,13 @@
     <head>
         <title>Recipebook</title>
         <link rel="stylesheet" href="/RecipeBook/Recipe-Book/css/home_for_all.css" type="text/css">
-        <link class="logo" rel="icon" href="/RecipeBook/Recipe-Book/logo/logo2.png" type="image/png">
+        <link rel="icon" href="/RecipeBook/Recipe-Book/logo/logo.png" type="image/png">
     </head>
 
     <body>
         <nav class="navbar">
             <div class="logo">
-                <img src="/RecipeBook/Recipe-Book/logo/logo2.png" onclick="about()" style="width: 150px; height: 200px; border-radius: 50%;"/>&nbsp;&nbsp;
+                <img src="/RecipeBook/Recipe-Book/logo/logo.png" onclick="about()" style="width: 150px; height: 150px;"/>
                 <h1 onclick="about()" class="recipebook">Recipebook</h1>
             </div>
 
@@ -125,17 +125,23 @@
         <!-- pop up box for about -->
         <div id="about" class="popup">
             <div class="popup_content">
-                <span class="close1" onclick="closePopup1()" style="font-size:30px; color:white;">&times;</span>
-                <h1 style="color:white;">About Recipebook</h1>
+                <div style="text-align:right;">
+                    <span class="close1" onclick="closePopup1()" style="font-size:35px; color:black;">&times;</span>
+                </div><br/>
+                <img src="/RecipeBook/Recipe-Book/logo/logo.png" style="width: 300px; height: 300px;"/>
+                <h1 style="color:black;">About Recipebook</h1>
+                <p style="font-size: 20px; text-align:left;">Recipebook is an online platform that serves as a social networking and content-sharing system which enables users to share their own recipes. It's an interactive and personalized place for food enthusiasts, with the ability to post recipes, like and comment on posts, and search for recipes by title, username, or ingredients they have on hand.</p>
             </div>
         </div>
 
         <!-- pop up box for signup and login -->
         <div id="signup_login_popup" class="popup">
             <div class="popup_content">
-                <span class="close2" onclick="closePopup2()" style="font-size:30px; color:white;">&times;</span><br/>
-                <button><a href="/RecipeBook/Recipe-Book/html/signup.html">Sign up</a></button>
-                <button><a href="/RecipeBook/Recipe-Book/html/login.html">Log in</a></button>
+            <div style="text-align:right;">
+                <span class="close2" onclick="closePopup2()" style="font-size:35px; color:black;">&times;</span><br/>
+            </div><br/>
+            <button><a href="/RecipeBook/Recipe-Book/html/signup.html">Sign up</a></button>
+            <button><a href="/RecipeBook/Recipe-Book/html/login.html">Log in</a></button>
             </div>
         </div>
 
