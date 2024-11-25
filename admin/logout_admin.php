1f0e75205d0da918cf43b5c301800b0f5ac66ebb
Rohan Kasichhwa <rohankasichhwa@gmail.com>
2024-11-25 08:45:26 +0545

    logout for admin

--- admin/logout_admin.php
+++ admin/logout_admin.php
@@ -2,8 +2,14 @@
     session_start();
 
     if ((isset($_SESSION['adminname']) && isset($_SESSION['admin_loggedin']) && isset($_SESSION['admin_id']))) {
-        session_unset();
-        session_destroy();
+
+        unset($_SESSION['admin_loggedin']);
+        unset($_SESSION['adminname']);
+        unset($_SESSION['admin_id']);
+
+        if (empty($_SESSION)) {
+            session_destroy();
+        }
         header("Location: /Recipebook/Recipe-Book/admin/login_admin.html");
         exit();
     } else {
